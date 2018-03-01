609e9fa6c7ed5273466495c1d0a8a01a21787063
Vincent <dev.vincentboulanger@gmail.com>
2018-03-01 14:01:56 +0100

    Initial Commit

---
+++ RPG-OPC-V6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RPG-OPC-V6
+//
+//  Created by VINCENT BOULANGER on 01/03/2018.
+//  Copyright © 2018 VBoulanger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
