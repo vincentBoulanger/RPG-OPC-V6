023c26ca0d527f875fe10c322e714a646d81ecfb
Vincent <dev.vincentboulanger@gmail.com>
2018-03-07 13:27:52 +0100

    Test des tableau multi dim avec des repeated et count ex: var arr = Array(count: 3, repeatedValue: Array(count: 2, repeatedValue: 0))

--- RPG-OPC-V6/Team/Team.swift
+++ RPG-OPC-V6/Team/Team.swift
@@ -14,15 +14,12 @@ class Team {
     var playerNumber = [Int]()
     var pseudoTab = [String]()
     
-    var teams = [Hero]()
-    var heroesTeam = [[Hero]]()
+    
   
    
-    /*
-    enum myHeroesGame2:Int {
-        case Warrior() = 1, Magician(), Dwarft(), Colossus()
- }
- */
+
+    
+ 
     let myHeroesGame = [Warrior(), Magician(), Dwarft(), Colossus()]
     
     var infiniteLoop = true
@@ -44,6 +41,11 @@ class Team {
         print("Soudain ! Vous vous retrouvez nez à nez avec une autre bande de mercenaires ! C'est l'affrontement ! La partie commence : ")
     }
     
+    var teamsMember = [Hero]()
+    var heroesTeam = [[Hero]]()
+    
+      let array = Array(repeating: [1,2,3], count: 3) // => [1, 2, 3, 1, 2, 3, 1, 2, 3]
+    //var arr = Array(count: 3, repeatedValue: Array(count: 2, repeatedValue: 0))
     
     func createTeam() { // create player/pseudo 1 and 2
         
@@ -63,52 +65,42 @@ class Team {
             print("====================")
             print("\(userPseudo), créez l'équipe \(i) :")
             print("====================")
-           
-    
-            var teams = [Hero]()
-            var heroesTeam = [[Hero]]()
-            heroesTeam.append(teams)
-for t in heroesTeam {
+         
+            heroesTeam.count
             
-            for j in 1...3 {
+            for i in 1...3 {
                 
                 descriptionHeroDisplay()
                 
                 let userChoiceTeam = myInputInt()
                 
                 switch userChoiceTeam {
-                    
                     case 1:
                         print("\(userPseudo) Vous avez ajouté un guerrier)")
-                        teams.append(Warrior())
+                       teamsMember[...].append(Warrior())
                     case 2:
                         print("\(userPseudo) Vous avez ajouté un magicien)")
-                       teams.append(Magician())
+                       teamsMember[...].append(Magician())
                     case 3:
                         print("\(userPseudo) Vous avez ajouté un nain)")
-                       teams.append(Dwarft())
+                       teamsMember[...].append(Dwarft())
                     case 4:
                         print("\(userPseudo) Vous avez ajouté un colosse)")
-                        teams.append(Colossus())
+                        teamsMember[...].append(Colossus())
                     default:
                         print("Veuillez taper un chiffre entre 1 et 4")
                 }
-                print("\(userPseudo), vous avez \(j) / 3 héros dans votre équipe.")
+                        print("\(userPseudo), vous avez \(i) / 3 héros dans votre équipe.")
                 }
-     heroesTeam.append(teams)
-            }
-            
-           
-
-
-         //   print("print heroes team Index 0   @@@@@@@@@@@@@@@@@@@ \(heroesTeam[0].count)")
-          //  print("print heroes team index 1  @@@@@@@@@@@@@@@@@@@ \(heroesTeam[1].count)")
-print("print Heroes Team   @@@@@@@@@@@@@ \(heroesTeam)")
+            heroesTeam[...].append(teamsMember)
          }
         
-        //print("print Heroes Team index 0   @@@@@@@@@@@@@ \(heroesTeam[0])")
-        //  print("print Heroes Team index 1 @@@@@@@@@@@@@@\(heroesTeam[1])")
+        print("print heroes team Index 0   @@@@@@@@@@@@@@@@@@@ \(heroesTeam[0].count)")
+        print("print heroes team index 1  @@@@@@@@@@@@@@@@@@@ \(heroesTeam[1].count)")
+       //print("print heroes team index 2  @@@@@@@@@@@@@@@@@@@ \(heroesTeam[2].count)")
+        print("print Heroes Team   @@@@@@@@@@@@@ \(heroesTeam)")
     }
+ 
     
         func descriptionHeroDisplay() {
             var heroNumber = 0
@@ -117,6 +109,7 @@ print("print Heroes Team   @@@@@@@@@@@@@ \(heroesTeam)")
                 heroNumber += 1
                 print("\(heroNumber) - Le \( i.descriptionClassHero) - Vie : \(i.lifePoints) - Attaque : \(i.attackPointsHero) - Soins : \(i.healHeroPoints)")
                     }
+            print("Choisissez un chiffre entre 1 et 4.")
             print("================================================================================")
                                         }
   
