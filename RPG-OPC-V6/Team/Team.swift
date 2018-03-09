f7d7575057416a973134e37485227a0fa57554c9
Vincent <dev.vincentboulanger@gmail.com>
2018-03-09 17:01:28 +0100

    The class team ended and was commented on.

--- RPG-OPC-V6/Team/Team.swift
+++ RPG-OPC-V6/Team/Team.swift
@@ -11,10 +11,10 @@ import Foundation
 
 class Team {
     
-    var heroes = [Hero]()
+    var heroes = [Hero]() // create empty tab hero
 
     
-    func describeHeroMenu(){
+    func describeHeroMenu(){ // selection hero menu display
 
         print("================================================================================")
         print("1 - Guerrier - Vie: 100 - Attaque: 10")
@@ -28,44 +28,45 @@ class Team {
     func createHeroes() { // create player/pseudo 1 and 2, cette fonction remplit le tab ligne 14 avec 3 PERSO
         
                 let userPseudoPlayer = myInputString()
-        
                 print("Vous vous appelerez désormais \(userPseudoPlayer)")
         
         for _ in 1...3 {
-            var userChoiceTeam = 0
-            describeHeroMenu()
-            repeat {
-               userChoiceTeam = myInputInt()
-        } while userChoiceTeam != 1 && userChoiceTeam != 2 && userChoiceTeam != 3 && userChoiceTeam != 4
             
-                    switch userChoiceTeam {
+            var userChoiceTeam = 0
+            describeHeroMenu() // call the function describe Hero Menu
+            
+            repeat {
+               userChoiceTeam = myInputInt() // input selection hero's player
+                
+            } while userChoiceTeam != 1 && userChoiceTeam != 2 && userChoiceTeam != 3 && userChoiceTeam != 4
+            
+                    switch userChoiceTeam { // the player's choices are compared with the prpposition's game
                         
                         case 1:
                             print("\(userPseudoPlayer) Vous avez ajouté un guerrier.")
                             heroes.append(Warrior())
-                        //namingHero()
+                  
                         case 2:
                             print("\(userPseudoPlayer) Vous avez ajouté un colosse.")
                             heroes.append(Colossus())
-                        //namingHero()
+              
                         case 3:
                             print("\(userPseudoPlayer) Vous avez ajouté un nain.")
                             heroes.append(Dwarft())
-                        //namingHero()
+          
                         case 4:
                             print("\(userPseudoPlayer) Vous avez ajouté un magicien.")
                             heroes.append(Magician())
-                       // namingHero()
+          
                         default:
                            return
-                            print("Veuillez entrer un chiffre entre 1 et 3")
+                        }
                     }
-            //} while userChoiceTeam == 1 || userChoiceTeam == 2 || userChoiceTeam == 3 || userChoiceTeam == 4
-        
-        }
-    namingHero()
-    }
-    func namingHero() {
+                    namingHero() // call the function to rename the heroes
+            }
+    
+    
+    func namingHero() { // function to rename the heroes choose by the players
             for i in heroes {
                     print("")
                     print("Choisissez un pseudo pour votre : \(i.descriptionClassHero)")
@@ -75,30 +76,23 @@ class Team {
                     print(" Votre \(i.descriptionClassHero ) s'appelle  \(i.nameHero)" )
             }
 }
-    
-    
-    
+
     // ##############################  Mes inputs ##################################
     
     
-    func myInputString() -> String {
+    func myInputString() -> String { // function readLine unwrapped / verified to return a String
         guard let str = readLine() else { return "" }
         return str
     }
     
-    func myInputInt() -> Int{
+    func myInputInt() -> Int{ // function readLine unwrapped / verified and converted ti return an Integer
         guard let input = readLine() else {return 0}
         guard let inputToInt = Int(input) else {return 0}
         return inputToInt
     }
 }
-    
-    
-    
-    
-    
-    
 
+    // #############################################################################
 //       func teamDescription() { // faire un print
 //            var heroNumber = 0
 //            print("================================================================================")
